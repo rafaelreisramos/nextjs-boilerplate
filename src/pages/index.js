41d2e8ef25a9aeb8ba7d8ad4a6d2892afd74bce0
rafaelreisramos <rrramos@gmail.com>
2020-12-08 09:31:43 -0300

    App cleaned up.

---
+++ src/pages/index.js
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>
+          Welcome to Next.js!
+        </h1>
+      </main>
+    </>
+  )
+}
