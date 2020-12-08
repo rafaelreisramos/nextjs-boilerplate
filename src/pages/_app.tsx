da63e45946d022abcb459692d9f386514421647a
rafaelreisramos <rrramos@gmail.com>
2020-12-08 10:03:47 -0300

    Add Typescript support

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
+import { AppProps } from 'next/app'
 import '../styles/globals.css'
 
-function App({ Component, pageProps }) {
+function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
 
