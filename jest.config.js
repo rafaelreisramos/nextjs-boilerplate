b1a18e9f9e889c5e60882939124a9c7a3d8872ce
rafaelreisramos <rrramos@gmail.com>
2020-12-10 09:33:26 -0300

    feat: jest setup added

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+const { name } = require('./package.json')
+
+module.exports = {
+  displayName: name,
+  name,
+  testEnvironment: 'jsdom',
+  testMatch: ['<rootDir>/src/**/?(*.)+(spec|test).[tj]s?(x)'],
+  testPathIgnorePatterns: ['/node_modules/', './next'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts(x)?'],
+  coverageDirectory: '<rootDir>/__coverage__',
+  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts']
+}
