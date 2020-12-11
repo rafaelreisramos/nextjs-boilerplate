f19b2bf9924bd1244d92132bba7f67fe37a511c3
rafaelreisramos <rrramos@gmail.com>
2020-12-10 23:55:09 -0300

    feat: added styled-components ThemeProvider

--- babel.config.js
+++ babel.config.js
@@ -8,5 +8,5 @@ module.exports = {
         displayName: true
       }
     ]
-  ],
+  ]
 }
--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -2,9 +2,7 @@ import * as S from './styles'
 
 const Main = () => (
   <S.Container>
-    <S.Title>
-      Welcome to Next.js boilerplate!
-    </S.Title>
+    <S.Title>Welcome to Next.js boilerplate!</S.Title>
   </S.Container>
 )
 
